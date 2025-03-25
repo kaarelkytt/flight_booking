97cc99894f47a914cd4f594c32b8019ac3882b6c
kaarelkytt <56921137+kaarelkytt@users.noreply.github.com>
2025-03-26 01:55:16 +0200

    Refactor

--- backend/src/main/java/org/example/flight_booking/controller/SeatController.java
+++
@@ -1,9 +0,0 @@
-package org.example.flight_booking.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/seats")
-public class SeatController {
-}
