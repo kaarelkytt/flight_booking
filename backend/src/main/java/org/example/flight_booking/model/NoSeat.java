ccbea90ba13bdfb02e570dc96baae719a03f05ec
kaarelkytt <56921137+kaarelkytt@users.noreply.github.com>
2025-03-26 03:52:42 +0200

    Update NoSeat.java

--- backend/src/main/java/org/example/flight_booking/model/NoSeat.java
+++ backend/src/main/java/org/example/flight_booking/model/NoSeat.java
@@ -2,6 +2,6 @@ package org.example.flight_booking.model;
 
 public class NoSeat extends Seat{
     public NoSeat() {
-        super("_", true, false, false, false, false);
+        super(null, true, false, false, false, false);
     }
 }
